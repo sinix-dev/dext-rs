336b37844650b7e018f32248afb55999c14bf191
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-08-14 01:31:36 +0530

    feat: add dext builder and writer

--- dext/src/builder/mod.rs
+++ dext/src/builder/mod.rs
@@ -2,20 +2,82 @@ extern crate html_minifier;
 extern crate hyper;
 extern crate memmap;
 extern crate walkdir;
+extern crate lexical_sort;
 
-use fst::MapBuilder;
+use fst::{MapBuilder,Map};
 use std::fs::read;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{Write, Result as IOResult};
 use std::io::{BufWriter, Error};
 use std::path::Path;
 use walkdir::WalkDir;
+use memmap::MmapOptions;
+use lexical_sort::{StringSort, natural_lexical_cmp};
 
-pub fn pack_in_u64(offset: usize, length: usize) -> u64 {
-  (length as u64) | ((offset as u64) << 32)
+#[derive(Debug)]
+struct DextBuilder {
+  name: String,
+  files: Vec<String>,
+  buffer: Option<BufWriter<File>>,
+  writer: DextWriter,
+}
+
+#[derive(Clone,Debug)]
+struct DextWriter {
+  content: Vec<u8>
+}
+
+impl Write for DextWriter {
+  // write into the archive
+  fn write(self: &mut Self, buf: &[u8]) -> IOResult<usize> {
+    self.content.extend(buf);
+    Ok(buf.len())
+  }
+
+  fn flush(&mut self) -> IOResult<()> {
+    Ok(())
+  }
+}
+
+impl DextWriter {
+  fn new() -> Self {
+    DextWriter { content: Vec::new() }
+  }
+}
+
+impl DextBuilder {
+  fn add(self: Self, content: &str) -> DextBuilder {
+    println!("SELF: {}", content);
+    self
+  }
 }
 
 pub fn build(src: &Path, target: &Path) -> Result<(), Error> {
+  let dext = DextBuilder {
+    name: String::from("app"),
+    files: Vec::new(),
+    buffer: None,
+    writer: DextWriter::new()
+  };
+
+  // dext.add("hello");
+
+  let mut writer_clone = dext.writer.clone();
+  let mut build = MapBuilder::new(writer_clone).unwrap();
+
+  build.insert("bruce", 1).unwrap();
+  build.insert("clarence", 2).unwrap();
+  build.insert("stevie", 3).unwrap();
+
+  build.finish().unwrap();
+
+  let content = dext.writer.content;
+  println!("{:?}", content);
+  // let map = Map::new(content).unwrap();
+  // println!("{:?}", map.len());
+
+  return Ok(());
+
   let dext_path = target.with_extension("dext");
   let index_path = target.with_extension("index");
   let archive_path = target.with_extension("archive");
@@ -28,28 +90,40 @@ pub fn build(src: &Path, target: &Path) -> Result<(), Error> {
     let mut offset = 0;
 
     dext.write_all(String::from("Dext v1").as_bytes()).unwrap();
+    let mut files: Vec<String> = WalkDir::new(src).into_iter().map(|e| String::from(e.unwrap().path().to_str().unwrap())).collect();
+    println!("{:?}", files);
+    files.string_sort_unstable(natural_lexical_cmp);
+    println!("{:?}", files);
 
-    for entry in WalkDir::new(src).into_iter().filter_map(|e| e.ok()) {
-      if entry.path().is_file() {
-        let content = get_compressed_content(entry.path()).unwrap();
-        let off = pack_in_u64(offset, content.len());
+    for entry in files.into_iter() {
+      let path = Path::new(&entry);
+      if path.is_file() {
+        println!("{:?}", path);
+        let content = read(path).unwrap();
+        // let content = get_compressed_content(path).unwrap();
 
-        archive.write_all(&content).unwrap();
-        index
-          .insert(
-            &entry
-              .path()
-              .strip_prefix(src)
-              .unwrap()
-              .to_str()
-              .unwrap()
-              .as_bytes(),
-            off,
-          )
-          .unwrap();
-
-        offset += content.len();
+        println!("{:?}", std::str::from_utf8(&content).unwrap());
       }
+      // if entry.path().is_file() {
+      //   let content = get_compressed_content(entry.path()).unwrap();
+      //   let off = pack_in_u64(offset, content.len());
+
+      //   archive.write_all(&content).unwrap();
+      //   index
+      //     .insert(
+      //       &entry
+      //         .path()
+      //         .strip_prefix(src)
+      //         .unwrap()
+      //         .to_str()
+      //         .unwrap()
+      //         .as_bytes(),
+      //       off,
+      //     )
+      //     .unwrap();
+
+      //   offset += content.len();
+      // }
     }
 
     dext.write_all(archive.buffer()).unwrap();
@@ -65,6 +139,8 @@ fn get_compressed_content(path: &Path) -> Result<Vec<u8>, Error> {
   use std::str;
 
   let data = read(path)?;
+
+  println!("{:?}", path);
 
   let data = match path.extension().unwrap().to_str().unwrap() {
     "js" => html_minifier::js::minify(str::from_utf8(&data).unwrap()),
